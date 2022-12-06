072ddce0acff382560a644fbec9f2344855d32a2
Griseliz <git-commit-address>
2022-12-05 20:48:15 -0400

    i hope it works

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Get todos from user."""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    clients_id = requests.get('https://jsonplaceholder.typicode.com/users/{}/todos'
+                              .format(argv[1]))
+    all = clients_id.json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(argv[1]))
+    user = user.json()
+    list = []
+    for todo in all:
+        if todo.get('completed'):
+            list.append(todo)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), len(list), len(all)))
+    for todo in list:
+        print("\t {}".format(todo.get('title')))
